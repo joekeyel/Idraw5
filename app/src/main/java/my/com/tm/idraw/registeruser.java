691f9501262384b9078a40190edd6a44411c370b
Hasanul Isyraf <hasanul2307@gmail.com>
2018-02-21 21:39:04 +0800

    setup vcs

--- app/src/main/java/my/com/tm/idraw/registeruser.java
+++ app/src/main/java/my/com/tm/idraw/registeruser.java
@@ -179,7 +179,7 @@ public class registeruser extends AppCompatActivity {
 
 
                 } else {
-                    //testkkk uh
+                    //testkkk uh hhjj
                     // Permission Denied
                     Toast.makeText(registeruser.this, "Pls Allow Permission To access full Feature", Toast.LENGTH_SHORT)
                             .show();
